33a1c9cd87e510964d7e67efd2c6b03b91cf269d
gil <ryogo5.0304@gmail.com>
2026-07-13 21:59:20 +0900

    Initial Commit

---
+++ SummerBottle/SummerBottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerBottleApp.swift
+//  SummerBottle
+//
+//  Created by 落合遼梧 on 2026/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SummerBottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
